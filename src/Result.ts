f1549f3123ad65dbf6819c5ed73a159f498c367d
alphaArgon <49616104+alphaArgon@users.noreply.github.com>
2025-03-18 00:50:35 +0800

    Enhance `Result` with `zip` and `zipErrors` methods

--- src/Result.ts
+++ src/Result.ts
@@ -9,23 +9,62 @@
 import { $ } from "./_Internal";
 
 
-
+/** Creates a successful result. */
 export function success<Success>(value: Success): Result<Success, never> {
-    let result = Reflect.construct(Object, [], Result) as Result<Success, never>;
-    result[$] = {value, failed: false};
-    return result;
+    return Object.freeze({
+        __proto__: Result.prototype,
+        [$]: Object.freeze({value, failed: false}),
+    }) as any;
 }
 
+/** Creates a failed result. */
 export function failure<Failure>(value: Failure): Result<never, Failure> {
-    let result = Reflect.construct(Object, [], Result) as Result<never, Failure>;
-    result[$] = {value, failed: true};
-    return result;
+    return Object.freeze({
+        __proto__: Result.prototype,
+        [$]: Object.freeze({value, failed: true}),
+    }) as any;
+}
+
+
+/** Combines multiple results into a single tuple result. If any of the them failed, the returned
+  * result will be the first failure. */
+export function zip<Successes extends any[], Failure>(
+    ...results: {[I in keyof Successes]: Result<Successes[I], Failure>}
+): Result<Successes, Failure> {
+    for (let result of results) {
+        if (result[$].failed) {
+            return result as Result<any, Failure>;
+        }
+    }
+
+    return success(results.map(result => result[$].value) as Successes);
+}
+
+
+/** Combines multiple results into a single tuple result. If any of the them failed, the returned
+  * result will combine all the failures. */
+export function zipErrors<Successes extends any[], Failure>(
+    ...results: {[I in keyof Successes]: Result<Successes[I], Failure>}
+): Result<Successes, Failure[]> {
+    let failures: Failure[] = [];
+
+    for (let result of results) {
+        if (result[$].failed) {
+            failures.push(result[$].value as Failure);
+        }
+    }
+
+    if (failures.length !== 0) {
+        return failure(failures);
+    }
+
+    return success(results.map(result => result[$].value) as Successes);
 }
 
 
 export class Result<Success, Failure> {
 
-    [$]: {
+    readonly [$]: {
         readonly value: Success | Failure;
         readonly failed: boolean;
     };
